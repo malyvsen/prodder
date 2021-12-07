3bdb859af6142cb70de11404ff4c81b90dc299bd
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-12-07 11:40:20 +0100

    fix: streamlit warnings related to pot

--- prodder/components/chip_counter.py
+++ prodder/components/chip_counter.py
@@ -3,30 +3,41 @@ import streamlit as st
 
 
 def chip_counter() -> int:
-    variable_name = "num_chips"
-    current_key = "num_chips_current"
-    added_key = "num_chips_added"
-    if variable_name not in st.session_state:
-        st.session_state[variable_name] = 0
+    pot_state_key = "num_chips_pot"
+    pot_widget_key = "pot_widget"
+    if pot_state_key not in st.session_state:
+        st.session_state[pot_state_key] = 0
+    added_state_key = "num_chips_added"
+    added_widget_key = "added_widget"
+    if added_state_key not in st.session_state:
+        st.session_state[added_state_key] = 0
 
-    current_column, new_column = st.columns(2)
+    pot_column, added_column = st.columns(2)
 
-    def set_chips():
-        st.session_state[variable_name] = st.session_state[current_key]
+    def set_pot():
+        st.session_state[pot_state_key] = st.session_state[pot_widget_key]
 
-    current_column.number_input(
-        "Number of chips in the pot",
-        value=st.session_state[variable_name],
-        on_change=set_chips,
-        key=current_key,
-    )
+    def set_added():
+        st.session_state[added_state_key] = st.session_state[added_widget_key]
 
-    def add_chips():
-        st.session_state[variable_name] += st.session_state[added_key]
-        st.session_state[added_key] = 0
+    def add_to_pot():
+        st.session_state[pot_state_key] += st.session_state[added_state_key]
+        st.session_state[added_state_key] = 0
 
-    with new_column:
-        st.number_input("Number of chips being added", value=0, key=added_key)
-        st.button("Add these", on_click=add_chips)
+    with pot_column:
+        st.number_input(
+            "Chips in the pot",
+            value=st.session_state[pot_state_key],
+            on_change=set_pot,
+            key=pot_widget_key,
+        )
+    with added_column:
+        st.number_input(
+            "Chips an opponent is betting",
+            value=st.session_state[added_state_key],
+            on_change=set_added,
+            key=added_widget_key,
+        )
+        st.button("Add these to the pot", on_click=add_to_pot)
 
-    return st.session_state[variable_name]
+    return st.session_state[pot_state_key]
